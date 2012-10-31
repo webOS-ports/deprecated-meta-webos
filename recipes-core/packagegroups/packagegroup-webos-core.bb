ka6sox/meta-webos

packagegroup: inherit packagegroup and drop PACKAGES and ALLOW_EMPTY settings
Open-webOS-DCO-1.0-Signed-off-by: Martin Jansa <Martin.Jansa@gmail.com>

recipes-core/packagegroups/packagegroup-webos-core.bb
@@ -4,17 +4,11 @@ DESCRIPTION = "openembedded-core and meta-oe components used in Open webOS"
 LICENSE = "Apache-2.0"
 LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/Apache-2.0;md5=89aea4e17d99a7cacdbeed46a0096b10"
 
-PR = "r2"
+PR = "r3"
 
-PACKAGES = "\
-    ${PN} \
-    ${PN}-dbg \
-    ${PN}-dev \
-    "
+inherit packagegroup
 
 PACKAGE_ARCH = "${MACHINE_ARCH}"
-
-ALLOW_EMPTY = "1"
 
 RPROVIDES_${PN} = "task-webos-core"
 RREPLACES_${PN} = "task-webos-core"

webOS-ports/deprecated-meta-webos

recipes-core/packagegroups/packagegroup-webos-core.bb
@@ -4,17 +4,11 @@ DESCRIPTION = "openembedded-core and meta-oe components used in Open webOS"
 LICENSE = "Apache-2.0"
 LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/Apache-2.0;md5=89aea4e17d99a7cacdbeed46a0096b10"
 
-PR = "r6"
+PR = "r7"
 
-PACKAGES = "\
-    ${PN} \
-    ${PN}-dbg \
-    ${PN}-dev \
-    "
+inherit packagegroup
 
 PACKAGE_ARCH = "${MACHINE_ARCH}"
-
-ALLOW_EMPTY = "1"
 
 RPROVIDES_${PN} = "task-webos-core"
 RREPLACES_${PN} = "task-webos-core"